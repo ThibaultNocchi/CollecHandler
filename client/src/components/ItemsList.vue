dcf0d9775f0393860f4fe0b8b4f3d59cdfc26a04
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2022-01-27 22:11:45 +0100

    put add item logic to collection page

--- client/src/components/ItemsList.vue
+++ client/src/components/ItemsList.vue
@@ -10,28 +10,12 @@
 
 	<h3>Search</h3>
 	<SearchForm />
-
-	<h3>Add new item</h3>
-	<form @submit.prevent="onSubmit">
-		<input type="text" placeholder="Name" v-model="form.name" />
-		<br />
-		<input type="number" min="0" placeholder="Quantity" v-model="form.quantity" />
-		<br />
-		<input type="text" placeholder="Barcode" v-model="form.barcode" />
-		<BarcodeScanner @change="onNewItemBarcodeChange" />
-		<br />
-		<textarea placeholder="Description" v-model="form.description"></textarea>
-		<br />
-		<input type="submit" />
-	</form>
 </template>
 
 <script setup lang="ts">
-import { useAddItemMutation, useSearchItemsQuery } from '@/graphql/graphql';
-import { computed, reactive } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
+import { useSearchItemsQuery } from '@/graphql/graphql';
+import { computed } from 'vue';
 import Pagination from './Pagination.vue';
-import BarcodeScanner from './BarcodeScanner.vue';
 import SearchForm from './SearchForm.vue';
 import searchQueryStore from '@/plugins/searchQuery';
 
@@ -41,9 +25,6 @@ const props = defineProps({
 		required: true
 	}
 })
-
-const router = useRouter()
-const route = useRoute()
 
 const searchQuery = await useSearchItemsQuery({
 	variables: {
@@ -68,36 +49,7 @@ const searchQuery = await useSearchItemsQuery({
 	}
 })
 
-const addItem = useAddItemMutation()
 const items = computed(() => searchQuery.data.value?.search.items || [])
 const pages = computed(() => searchQuery.data.value?.search.pages || 1)
-
-const form = reactive({
-	name: '',
-	quantity: 1,
-	barcode: '',
-	description: ''
-})
-
-
-const onSubmit = async () => {
-	const addItemMutation = await addItem.executeMutation({
-		collectionId: props.collectionId, input: {
-			name: form.name,
-			quantity: form.quantity,
-			barcode: form.barcode || undefined,
-			description: form.description || undefined
-		}
-	}, { additionalTypenames: ['Collection'] })
-	if (addItemMutation.data?.addItem?.id)
-		router.push({ name: 'Item', params: { collectionId: props.collectionId, id: addItemMutation.data.addItem.id } })
-	else
-		alert('error adding item')
-}
-
-const onNewItemBarcodeChange = (val: string | undefined) => {
-	if (val) form.barcode = val
-	else alert('Can\'t read barcode, try again')
-}
 
 </script>
