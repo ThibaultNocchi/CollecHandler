b1dfb71f5f781304113b99e59f23a3675f0f06f0
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2022-01-30 20:20:03 +0100

    Migrated to a component for the FS cards

--- client/src/pages/Login.vue
+++ client/src/pages/Login.vue
@@ -1,39 +1,31 @@
 <template>
-	<v-row align="center" justify="center">
-		<v-col sm="10" lg="6" xl="4" class="text-center">
-			<v-card>
-				<v-card-header>
-					<v-card-header-text>
-						<v-card-title>Login</v-card-title>
-						<v-card-subtitle>Login to your CollecHandler instance to manage your collections</v-card-subtitle>
-					</v-card-header-text>
-				</v-card-header>
-				<v-card-text>
-					<v-form class="text-end" @submit.prevent="onSubmit">
-						<v-text-field v-model="form.pseudo" autofocus hide-details label="Pseudo" prepend-inner-icon="mdi-account" />
-						<v-text-field
-							v-model="form.password"
-							dense
-							hide-details
-							label="Password"
-							prepend-inner-icon="mdi-key"
-							:type="showPassword ? 'text' : 'password'"
-						>
-							<template #append class="pointer">
-								<v-icon @click="showPassword = !showPassword" class="pointer">{{ showPassword ? "mdi-eye" : "mdi-eye-off" }}</v-icon>
-							</template>
-						</v-text-field>
-						<v-checkbox v-model="form.rememberMe" color="primary" label="Remember me" hide-details />
-						<v-btn color="primary" text block @click="onSubmit">Login</v-btn>
-					</v-form>
-				</v-card-text>
-			</v-card>
+	<FsCard>
+		<template #title>Login</template>
+		<template #subtitle>Login to your CollecHandler instance to manage your collections</template>
+		<v-form class="text-end" @submit.prevent="onSubmit">
+			<v-text-field v-model="form.pseudo" autofocus hide-details label="Pseudo" prepend-inner-icon="mdi-account" />
+			<v-text-field
+				v-model="form.password"
+				dense
+				hide-details
+				label="Password"
+				prepend-inner-icon="mdi-key"
+				:type="showPassword ? 'text' : 'password'"
+			>
+				<template #append class="pointer">
+					<v-icon @click="showPassword = !showPassword" class="pointer">{{ showPassword ? "mdi-eye" : "mdi-eye-off" }}</v-icon>
+				</template>
+			</v-text-field>
+			<v-checkbox v-model="form.rememberMe" color="primary" label="Remember me" hide-details />
+			<v-btn color="primary" text block @click="onSubmit">Login</v-btn>
+		</v-form>
+		<template #append>
 			<h3 class="mt-4">
 				Don't have an account?
 				<router-link :to="{ name: 'Register' }">Sign up</router-link>
 			</h3>
-		</v-col>
-	</v-row>
+		</template>
+	</FsCard>
 </template>
 
 <script setup lang="ts">
@@ -41,6 +33,7 @@
 import { setJwt } from '@/graphql/client';
 import { useLoginMutation } from '@/graphql/graphql';
 import { reactive, ref } from 'vue';
+import FsCard from '@/components/FsCard.vue'
 
 const form = reactive({
 	pseudo: '',
