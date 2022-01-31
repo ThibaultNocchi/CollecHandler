af30ab2ce0efb356254c77ca799d75278136c140
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2022-01-31 23:17:11 +0100

    add nice logo on login pages

--- client/src/components/FsCard.vue
+++ client/src/components/FsCard.vue
@@ -1,22 +1,18 @@
 <template>
-	<v-row align="center" justify="center">
-		<v-col sm="10" lg="6" xl="4" class="text-center">
-			<v-card class="mb-4">
-				<v-card-header>
-					<v-card-header-text>
-						<v-card-title>
-							<slot name="title" />
-						</v-card-title>
-						<v-card-subtitle>
-							<slot name="subtitle" />
-						</v-card-subtitle>
-					</v-card-header-text>
-				</v-card-header>
-				<v-card-text>
-					<slot />
-				</v-card-text>
-			</v-card>
-			<slot name="append" />
-		</v-col>
-	</v-row>
+	<v-card class="mb-4">
+		<v-card-header>
+			<v-card-header-text>
+				<v-card-title>
+					<slot name="title" />
+				</v-card-title>
+				<v-card-subtitle>
+					<slot name="subtitle" />
+				</v-card-subtitle>
+			</v-card-header-text>
+		</v-card-header>
+		<v-card-text>
+			<slot />
+		</v-card-text>
+	</v-card>
+	<slot name="append" />
 </template>
--- client/src/layouts/FullscreenLayout.vue
+++ client/src/layouts/FullscreenLayout.vue
@@ -1,10 +1,23 @@
 <template>
 	<v-main>
 		<v-container class="fsLayout d-flex">
-			<slot />
+			<v-row align="center" justify="space-around">
+				<v-col class="d-none d-lg-block" lg="6" xl="4">
+					<v-img class="mx-auto" :src="logo" style="width: 20vw;" />
+					<h1 class="text-center mt-4">CollecHandler</h1>
+				</v-col>
+				<v-col sm="10" lg="6" xl="4" class="text-center">
+					<slot />
+				</v-col>
+			</v-row>
 		</v-container>
 	</v-main>
 </template>
+
+<script lang="ts" setup>
+import logo from '../../logo.svg'
+console.log(logo)
+</script>
 
 <style scoped>
 .fsLayout {
