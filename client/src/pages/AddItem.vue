9c2872c88102f5d7fda17d8b1b80151a039d6597
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2022-02-02 22:01:52 +0100

    add new collection field

--- client/src/pages/AddItem.vue
+++ client/src/pages/AddItem.vue
@@ -8,6 +8,20 @@
 		<v-card-text>
 			<v-form>
 				<v-row>
+					<v-col cols="12" sm="6">
+						<label>Collection</label>
+						<select
+							v-model="form.collectionId"
+							class="pa-2 pointer"
+							style="width:100%;border:1px solid gray;border-radius: 5px;"
+						>
+							<option :value="undefined">New collection</option>
+							<option v-for="collection in collections" :value="collection.id">{{ collection.name }}</option>
+						</select>
+					</v-col>
+					<v-col v-if="!form.collectionId" cols="12" sm="6">
+						<v-text-field v-model="form.newCollectionName" hide-details label="New collection name" />
+					</v-col>
 					<v-col cols="12">
 						<v-text-field v-model="form.name" hide-details label="Item name" prepend-inner-icon="mdi-alphabetical" />
 					</v-col>
@@ -30,6 +44,9 @@
 					<v-col cols="12">
 						<v-textarea v-model="form.description" hide-details auto-grow label="Description" />
 					</v-col>
+					<v-col cols="12" class="text-right">
+						<v-btn color="primary" text>Send</v-btn>
+					</v-col>
 				</v-row>
 			</v-form>
 		</v-card-text>
@@ -38,9 +55,15 @@
 
 <script lang="ts" setup>
 import BarcodeScanner from '@/components/BarcodeScanner.vue';
-import { reactive } from 'vue';
+import { useMeQuery } from '@/graphql/graphql';
+import { computed, reactive } from 'vue';
+
+const me = await useMeQuery()
+const collections = computed(() => me.data.value?.me?.collections)
 
 const form = reactive({
+	collectionId: undefined,
+	newCollectionName: '',
 	name: '',
 	quantity: 1,
 	barcode: '',
