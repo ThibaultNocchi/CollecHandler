5dfe5eecc5cbf20eed3e669705b774eb79483af8
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2022-02-03 21:58:01 +0100

    delete creation of collection from collection page

--- client/src/components/CollectionsList.vue
+++ client/src/components/CollectionsList.vue
@@ -5,30 +5,13 @@
 			<li>{{ collection.name }}</li>
 		</router-link>
 	</ul>
-	<h3>Actions</h3>
-	<form @submit.prevent="onAdd">
-		<input type="text" placeholder="Collection name" v-model="newCollectioName" />
-		<br />
-		<input type="submit" value="Create collection" />
-	</form>
 </template>
 
-<script setup lang="ts">import { useAddCollectionMutation, useMeQuery } from '@/graphql/graphql';
-import { computed, ref } from 'vue';
-import { useRouter } from 'vue-router';
+<script setup lang="ts">
+import { useMeQuery } from '@/graphql/graphql';
+import { computed } from 'vue';
 const me = await useMeQuery()
-const addCollection = useAddCollectionMutation()
 
 const collections = computed(() => me.data.value?.me?.collections)
-const newCollectioName = ref('')
 
-const router = useRouter()
-
-const onAdd = async () => {
-	const res = await addCollection.executeMutation({ input: { name: newCollectioName.value } }, { additionalTypenames: ['User'] })
-	if (res.data?.addCollection?.id)
-		router.push({ name: "Collection", params: { id: res.data.addCollection.id } })
-	else
-		alert("error adding collection")
-}
 </script>
