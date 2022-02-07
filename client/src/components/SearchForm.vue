d740886ffe6bd0feb33760050820486675bd7883
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2022-02-07 08:54:59 +0100

    add debouncing on text search

--- client/src/components/SearchForm.vue
+++ client/src/components/SearchForm.vue
@@ -2,7 +2,7 @@
 	<v-form>
 		<v-row>
 			<v-col cols="12">
-				<v-text-field v-model="form.text" label="Text search" hide-details prepend-inner-icon="mdi-alphabetical" />
+				<v-text-field v-model="text" label="Text search" hide-details prepend-inner-icon="mdi-alphabetical" />
 			</v-col>
 
 			<v-divider />
@@ -43,17 +43,20 @@
 <script lang="ts" setup>
 import { Comparisons } from '@/graphql/graphql';
 import searchQuery, { routeItemSearch } from '@/plugins/searchQuery';
-import { reactive, watch } from 'vue';
+import { reactive, ref, watch } from 'vue';
+import { useDebounce } from '@vueuse/core'
 import BarcodeScanner from './BarcodeScanner.vue';
 
+const text = ref(searchQuery.text.value)
 const form = reactive({
-	text: searchQuery.text.value,
+	text: useDebounce(text, 500),
 	barcode: searchQuery.barcode.value,
 	quantityComparison: searchQuery.quantityComparison.value,
 	quantity: searchQuery.quantity.value
 })
 
 watch(form, (val) => {
+	console.log(form.text)
 	routeItemSearch(val)
 })
 
