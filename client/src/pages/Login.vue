2dc3cb24bbaae310817564703e890b0900c8824b
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2022-03-24 23:39:45 +0100

    refactor login variables

--- client/src/pages/Login.vue
+++ client/src/pages/Login.vue
@@ -4,7 +4,7 @@
 		<template #subtitle>Login to your CollecHandler instance to manage your collections</template>
 		<v-form :disabled="login.loading.value" class="text-end" @submit.prevent="onSubmit">
 			<v-text-field
-				v-model="payload.pseudo"
+				v-model="pseudo"
 				class="mb-2"
 				autofocus
 				:error="isError"
@@ -12,7 +12,7 @@
 				label="Pseudo"
 				prepend-inner-icon="mdi-account"
 			/>
-			<PasswordField v-model="payload.password" class="mb-2" :error-messages="errors" label="Password" />
+			<PasswordField v-model="password" class="mb-2" :error-messages="errors" label="Password" />
 			<v-checkbox v-model="rememberMe" color="primary" label="Remember me" hide-details />
 			<v-btn :disabled="login.loading.value" type="submit" color="primary" text block>Login</v-btn>
 		</v-form>
@@ -28,21 +28,19 @@
 <script setup lang="ts">
 import { useMutation } from '@vue/apollo-composable'
 import { setJwt } from '@/graphql/client';
-import { computed, reactive, ref, Ref } from 'vue';
+import { computed, ref, Ref } from 'vue';
 import FsCard from '@/components/FsCard.vue'
 import PasswordField from '@/components/PasswordField.vue';
 import { LoginDocument, Errors } from '@/graphql/graphql2';
 
-const payload = reactive({
-	password: '',
-	pseudo: ''
-})
+const pseudo = ref('')
+const password = ref('')
 const rememberMe = ref(false)
 const errors: Ref<string | undefined> = ref(undefined)
 
 const isError = computed(() => !!errors.value)
 
-const login = useMutation(LoginDocument, { variables: payload })
+const login = useMutation(LoginDocument)
 login.onDone((res) => {
 	if (!res.data?.login?.token) throw Error
 	setJwt(res.data.login.token, rememberMe.value)
@@ -54,7 +52,7 @@ login.onError((err) => {
 
 const onSubmit = async () => {
 	errors.value = undefined
-	login.mutate()
+	login.mutate({ pseudo: pseudo.value, password: password.value })
 }
 
 </script>
