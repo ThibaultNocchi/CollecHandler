3ed1d45e5d16ecedfedc50709c52addcca72cb79
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2022-03-27 16:25:29 +0200

    remove urql from item page

--- client/src/pages/Item.vue
+++ client/src/pages/Item.vue
@@ -20,12 +20,13 @@
 	<input type="submit" @click.prevent="onDelete" value="Delete item" />
 </template>
 
-<script setup lang="ts">import { useDeleteItemMutation, useGetItemQuery } from '@/graphql/graphql';
+<script setup lang="ts">
 import { computed, onMounted } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import jsbarcode from 'jsbarcode'
 import copy from 'copy-to-clipboard'
-
+import { useApolloClient, useMutation } from '@vue/apollo-composable';
+import { DeleteItemDocument, GetItemDocument } from '@/graphql/graphql2';
 
 if (typeof useRoute().params.collectionId !== "string") throw "Bad parameter"
 const collectionId = parseInt(useRoute().params.collectionId as string)
@@ -33,14 +34,23 @@ const collectionId = parseInt(useRoute().params.collectionId as string)
 if (typeof useRoute().params.id !== "string") throw "Bad parameter"
 const itemId = parseInt(useRoute().params.id as string)
 
-const res = await useGetItemQuery({ variables: { collectionId, id: itemId } })
-const deleteItem = useDeleteItemMutation()
-const item = computed(() => res.data.value?.me?.collection?.item || undefined)
+const { client } = useApolloClient()
+const router = useRouter()
+
+const res = await client.query({ query: GetItemDocument, variables: { collectionId, id: itemId } })
+const item = computed(() => res.data.me?.collection?.item || undefined)
+
+const deleteItem = useMutation(DeleteItemDocument)
+deleteItem.onDone(() => {
+	router.push({ name: 'Collection', params: { id: collectionId } })
+})
+deleteItem.onError(err => {
+	console.error(err)
+	alert("deletion failed")
+})
 
 if (!item.value)
-	useRouter().replace({ name: 'Collection', params: { id: collectionId } })
-
-const router = useRouter()
+	router.replace({ name: 'Collection', params: { id: collectionId } })
 
 onMounted(() => {
 	if (item.value?.barcode)
@@ -56,11 +66,7 @@ const copyBarcode = () => {
 
 const onDelete = async () => {
 	const res = await confirm("Are you sure you want to delete this item?")
-	if (res) {
-		const mutRes = await deleteItem.executeMutation({ id: itemId })
-		if (mutRes.data?.deleteItem?.id) router.push({ name: 'Collection', params: { id: collectionId } })
-		else alert("deletion failed")
-	}
+	if (res) deleteItem.mutate({ id: itemId })
 }
 
 </script>
