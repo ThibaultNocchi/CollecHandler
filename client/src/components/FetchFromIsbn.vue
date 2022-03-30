8a2590b296d104cb1ae70d376064364c0c28b801
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2022-03-30 08:01:41 +0200

    use text field error message

--- client/src/components/FetchFromIsbn.vue
+++ client/src/components/FetchFromIsbn.vue
@@ -3,7 +3,8 @@
 		v-model="barcodeForm"
 		:disabled="loading"
 		clearable
-		hide-details
+		hide-details="auto"
+		:error-messages="error"
 		label="Fetch from ISBN"
 		prepend-inner-icon="mdi-barcode"
 		append-inner-icon="mdi-magnify"
@@ -17,7 +18,7 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { Ref, ref } from 'vue';
 import BarcodeScanner from './BarcodeScanner.vue';
 import { useApolloClient } from '@vue/apollo-composable';
 import { FetchFromIsbnDocument } from '@/graphql/graphql';
@@ -27,14 +28,16 @@ const { client } = useApolloClient()
 
 const barcodeForm = ref('')
 const loading = ref(false)
+const error: Ref<string | undefined> = ref(undefined)
 
 const fetchInfos = async (barcode: string) => {
+	error.value = undefined
 	if (barcode !== '') {
 		loading.value = true
 		const res = (await client.query({ query: FetchFromIsbnDocument, variables: { isbn: barcode } }))
 		loading.value = false
 		if (!res.data.fetchFromIsbn) {
-			alert("No ISBN found")
+			error.value = "No ISBN found"
 			return
 		}
 		emit("change", res.data.fetchFromIsbn)
