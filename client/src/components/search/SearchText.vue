2b74c5b8b790dae9579a0d59c613370d33362a7b
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2022-04-10 19:51:47 +0200

    formatting

--- client/src/components/search/SearchText.vue
+++ client/src/components/search/SearchText.vue
@@ -1,24 +1,32 @@
 <template>
-	<v-text-field v-model="text" clearable density="comfortable" hide-details label="Text search"
-		prepend-inner-icon="mdi-alphabetical" :variant="props.variant" />
+  <v-text-field
+    v-model="text"
+    clearable
+    density="comfortable"
+    hide-details
+    label="Text search"
+    prepend-inner-icon="mdi-alphabetical"
+    :variant="props.variant"
+  />
 </template>
 
 <script lang="ts" setup>
-import searchQuery, { routeItemTextSearch } from '@/plugins/searchQuery';
-import { PropType, ref } from 'vue';
-import { watchDebounced } from '@vueuse/core'
+import searchQuery, { routeItemTextSearch } from "@/plugins/searchQuery";
+import { PropType, ref } from "vue";
+import { watchDebounced } from "@vueuse/core";
 
 const props = defineProps({
-	variant: {
-		type: String as PropType<"filled" | "outlined" | "plain" | "contained" | "underlined">,
-		default: "filled"
-	}
-})
+  variant: {
+    type: String as PropType<
+      "filled" | "outlined" | "plain" | "contained" | "underlined"
+    >,
+    default: "filled"
+  }
+});
 
-const text = ref(searchQuery.text.value)
+const text = ref(searchQuery.text.value);
 
-watchDebounced(text, (val) => {
-	routeItemTextSearch(val)
-})
-
+watchDebounced(text, val => {
+  routeItemTextSearch(val);
+});
 </script>
